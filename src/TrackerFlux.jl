c2b18c2f4e36597ece86958d7a6c60725fc8d01a
Yao Lu <luyaocns@gmail.com>
2020-06-05 20:21:04 +0800

    init commit

--- src/TrackerFlux.jl
+++ src/TrackerFlux.jl
@@ -1,5 +1,42 @@
 module TrackerFlux
 
-# Write your package code here.
+using Flux, Tracker
+
+track(m) = fmap(x -> x isa AbstractArray ? Tracker.param(x) : x, m)
+
+function Flux.Optimise.update!(opt, xs::Tracker.Params, gs)
+    for x in xs
+        Flux.Optimise.update!(opt, x, gs[x])
+    end
+end
+  
+function Flux.Optimise.update!(opt, x, x̄)
+    Tracker.update!(x, -Flux.Optimise.apply!(opt, Tracker.data(x), Tracker.data(x̄)))
+end
+
+Flux.gradient(f, args...) = Tracker.gradient(f, args...)
+
+_truncate(x::AbstractArray) = Tracker.data(x)
+
+_truncate(x::Tuple) = _truncate.(x)
+
+truncate!(m::Flux.Recur) = (m.state = _truncate(m.state))
+
+truncate!(m) = foreach(truncate!, Flux.functor(m)[1])
+
+function Flux.destructure(m)
+    xs = []
+    fmap(m) do x
+        x isa AbstractArray && push!(xs, x)
+        return x
+    end
+    θ = vcat(vec.(Tracker.data.(xs))...)
+    re = p -> Flux._restructure(m, p)
+    return Tracker.param(θ), re
+end
+
+function __init__()
+    @eval Flux truncate! = $truncate!
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,40 @@
-using TrackerFlux
 using Test
+using Statistics
+using TrackerFlux
+using Tracker
+using Flux
 
 @testset "TrackerFlux.jl" begin
-    # Write your tests here.
+
+x = randn(Float32, 10, 1, 100)
+y = mean(x, dims = 1)
+
+model = Chain(LSTM(10, 100), LSTM(100, 1)) |> TrackerFlux.track
+θ, re = Flux.destructure(model)
+@test Flux.destructure(re(θ))[1] == θ
+
+model(x[:, :, 1])
+@test Tracker.istracked(model[1].state[1])
+Flux.truncate!(model)
+@test !Tracker.istracked(model[1].state[1])
+
+function loss(x, y)
+    xs = Flux.unstack(x, 3)
+    ys = Flux.unstack(y, 3)
+    ŷs = model.(xs)
+    l = 0f0
+    for t in 1:length(ŷs)
+        l += Flux.mse(ys[t], ŷs[t])
+    end
+    return l / length(ŷs)
+end
+
+opt = ADAMW(1e-3, (0.9, 0.999), 1e-4)
+
+cb = () -> Flux.reset!(model)
+
+Flux.@epochs 10 Flux.train!(loss, params(model), repeat([(x, y)], 100), opt, cb = cb)
+
+@test loss(x, y) < 0.02
+
 end
